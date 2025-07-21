cf7ab11ed427da87f2b0528c4407ddd689c4a72d
urihijiri <adgwtnp.3695@gmail.com>
2025-07-22 08:01:06 +0900

    音の範囲を変更

--- src/utils/noteUtils.js
+++ src/utils/noteUtils.js
@@ -3,8 +3,10 @@ export function getFrequency(note) {
   const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
   const n = noteNames.indexOf(note);
   if (n === -1) return 440;
-  // 2～7オクターブの範囲でランダム
-  const octave = Math.floor(Math.random() * (7 - 2 + 1)) + 2;
+  // A3(オクターブ3)～B7の範囲でランダム
+  const minOctave = 3;
+  const maxOctave = 7;
+  const octave = Math.floor(Math.random() * (maxOctave - minOctave + 1)) + minOctave;
   const midi = (octave + 1) * 12 + n;
   return 440 * Math.pow(2, (midi - 69) / 12);
 }
