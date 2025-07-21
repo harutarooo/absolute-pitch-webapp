71b045428d59f2cd116da35c27d9a8585079730a
urihijiri <adgwtnp.3695@gmail.com>
2025-07-21 22:17:50 +0900

    viteのpwaプラグインを使用

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/absolute-pitch-webapp/', // GitHub Pages用
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '絶対音感トレーニング',
+        short_name: '絶対音感',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1976d2',
+        description: '絶対音感トレーニングWebアプリ',
+        icons: [
+          {
+            src: '/absolute-pitch-webapp/icon-1024.png',
+            sizes: '192x192 512x512 1024x1024',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico,webmanifest}'],
+      },
+    }),
+  ],
 });
