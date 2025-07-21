1f7ea171d40882e2a002088f3b7e02852db84e0e
urihijiri <adgwtnp.3695@gmail.com>
2025-07-22 08:15:47 +0900

    スクロールできないように設定

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StartPage = ({ onStart }) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <h1>絶対音感トレーニング</h1>
+      <h1>絶対音感<br />トレーニング</h1>
       <button style={{ fontSize: '2rem', padding: '1rem 2rem' }} onClick={onStart}>
         スタート
       </button>
